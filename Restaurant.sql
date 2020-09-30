03ff8cd2fe2c1a7325da3db9d273256076295f44
SlavaMatveev <70895889+SlavaMatveev@users.noreply.github.com>
2020-09-30 11:51:11 +0300

    Update Restaurant.sql

--- Restaurant.sql
+++ Restaurant.sql
@@ -1,35 +1,21 @@
-﻿CREATE TABLE Сотрудники
-(
-  Код_сотрудника INT NOT NULL,
-  ФИО VARCHAR NOT NULL,
-  Возраст INT NOT NULL,
-  Пол VARCHAR NOT NULL,
-  Адрес VARCHAR NOT NULL,
-  Телефон INT NOT NULL,
-  Паспортные_данные VARCHAR NOT NULL,
-  PRIMARY KEY (Код_сотрудника)
-);
-
 CREATE TABLE Должности
 (
   Код_должности INT NOT NULL,
-  Наименование_должности VARCHAR NOT NULL,
+  Наименование_должности INT NOT NULL,
   Оклад INT NOT NULL,
-  Обязанности VARCHAR NOT NULL,
-  Требования VARCHAR NOT NULL,
-  Код_сотрудника INT NOT NULL,
-  PRIMARY KEY (Код_должности),
-  FOREIGN KEY (Код_сотрудника) REFERENCES Сотрудники(Код_сотрудника)
+  Обязанности INT NOT NULL,
+  Требования INT NOT NULL,
+  PRIMARY KEY (Код_должности)
 );
 
 CREATE TABLE Склад
 (
-  Срок_годности DATE NOT NULL,
+  Срок_годности INT NOT NULL,
   Код_ингредиента INT NOT NULL,
-  Наименование_ингредиента VARCHAR NOT NULL,
-  Дата_выпуска DATE NOT NULL,
+  Наименование_ингредиента INT NOT NULL,
+  Дата_выпуска INT NOT NULL,
   Объём INT NOT NULL,
-  Поставщик VARCHAR NOT NULL,
+  Поставщик INT NOT NULL,
   Стоимость INT NOT NULL,
   PRIMARY KEY (Код_ингредиента)
 );
@@ -37,37 +23,51 @@ CREATE TABLE Склад
 CREATE TABLE Меню
 (
   Объём_ингридиента_1 INT NOT NULL,
-  Время_приготовления DATE NOT NULL,
-  Наименование_блюда VARCHAR NOT NULL,
+  Время_приготовления INT NOT NULL,
+  Наименование_блюда INT NOT NULL,
   Код_блюда INT NOT NULL,
   Объём_ингридиента_2 INT NOT NULL,
   Объём_ингридиента_3 INT NOT NULL,
   Стоимость INT NOT NULL,
   Код_ингредиента INT NOT NULL,
-  Код_ингридиента_3Код_ингредиента INT,
-  Код_ингридиента_2Код_ингредиента INT,
-  Код_заказа INT NOT NULL,
-  Код_блюда_3Код_заказа INT NOT NULL,
+  Код_ингридиента_3Код_ингредиента INT NOT NULL,
+  Код_ингридиента_2Код_ингредиента INT NOT NULL,
   PRIMARY KEY (Код_блюда),
   FOREIGN KEY (Код_ингредиента) REFERENCES Склад(Код_ингредиента),
   FOREIGN KEY (Код_ингридиента_3Код_ингредиента) REFERENCES Склад(Код_ингредиента),
-  FOREIGN KEY (Код_ингридиента_2Код_ингредиента) REFERENCES Склад(Код_ингредиента),
-  FOREIGN KEY (Код_заказа) REFERENCES Заказ(Код_заказа),
-  FOREIGN KEY (Код_блюда_3Код_заказа) REFERENCES Заказ(Код_заказа)
+  FOREIGN KEY (Код_ингридиента_2Код_ингредиента) REFERENCES Склад(Код_ингредиента)
 );
 
 CREATE TABLE Заказ
 (
-  Время DATE NOT NULL,
-  Дата DATE NOT NULL,
-  Отметка_о_выполнении VARCHAR NOT NULL,
+  Время INT NOT NULL,
+  Дата INT NOT NULL,
+  Отметка_о_выполнении INT NOT NULL,
   Стоимость INT NOT NULL,
-  ФИО_заказчика VARCHAR NOT NULL,
+  ФИО_заказчика INT NOT NULL,
   Телефон INT NOT NULL,
   Код_заказа INT NOT NULL,
   Код_блюда INT NOT NULL,
-  Код_сотрудника INT NOT NULL,
+  Код_блюда_2Код_блюда INT NOT NULL,
+  Код_блюда_3Код_блюда INT NOT NULL,
   PRIMARY KEY (Код_заказа),
   FOREIGN KEY (Код_блюда) REFERENCES Меню(Код_блюда),
-  FOREIGN KEY (Код_сотрудника) REFERENCES Сотрудники(Код_сотрудника)
+  FOREIGN KEY (Код_блюда_2Код_блюда) REFERENCES Меню(Код_блюда),
+  FOREIGN KEY (Код_блюда_3Код_блюда) REFERENCES Меню(Код_блюда)
+);
+
+CREATE TABLE Сотрудники
+(
+  Код_сотрудника INT NOT NULL,
+  ФИО INT NOT NULL,
+  Возраст INT NOT NULL,
+  Пол INT NOT NULL,
+  Адрес INT NOT NULL,
+  Телефон INT NOT NULL,
+  Паспортные_данные INT NOT NULL,
+  Код_должности INT,
+  Код_заказа INT NOT NULL,
+  PRIMARY KEY (Код_сотрудника),
+  FOREIGN KEY (Код_должности) REFERENCES Должности(Код_должности),
+  FOREIGN KEY (Код_заказа) REFERENCES Заказ(Код_заказа)
 );
